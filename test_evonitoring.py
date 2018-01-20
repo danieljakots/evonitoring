96ea993014c37945f693900d5d81b673ae264e20
Daniel Jakots <vigdis@chown.me>
2018-01-20 00:30:57 -0500

    Update tests after recent commits

--- test_evonitoring.py
+++ test_evonitoring.py
@@ -4,14 +4,32 @@ import evonitoring
 class TestEvonitoring(unittest.TestCase):
 
     def test_readconf(self):
-        evonitoring.readconf()
-        self.assertEqual(evonitoring.cfg["twilio_available_number"],
+        oncallnumbers, cfg = evonitoring.readconf()
+        # api_cfg
+        self.assertEqual(evonitoring.api_cfg["twilio_available_number"],
                          "+14385556677")
-        self.assertEqual(evonitoring.cfg["pushover_active"], "True")
-        self.assertEqual(evonitoring.cfg["FR_sender"], "smsmode")
-        self.assertEqual(evonitoring.cfg["mobyt_sender"], "33609876543")
-        self.assertEqual(evonitoring.cfg["smsmode_pass"], "mcpasswordface")
-        self.assertEqual(evonitoring.cfg["irc_active"], "True")
+        self.assertEqual(evonitoring.api_cfg["pushover_user"], "johndoe")
+        self.assertEqual(evonitoring.api_cfg["mobyt_sender"],
+                         "33609876543")
+        self.assertEqual(evonitoring.api_cfg["smsmode_pass"],
+                         "mcpasswordface")
+        # cfg
+        self.assertEqual(cfg["pushover_active"], "True")
+        self.assertEqual(cfg["irc_active"], "True")
+        self.assertEqual(cfg["FR_sender"], "smsmode")
+        # oncallnumbers
+        self.assertIsInstance(oncallnumbers, list)
+        self.assertEqual(''.join(oncallnumbers[0]), "33612345678")
+
+
+    def test_decide_alerting(self):
+        oncallnumbers, cfg = evonitoring.readconf()
+        self.assertEqual(evonitoring.decide_alerting("33612345678", cfg),
+                         "smsmode")
+        self.assertEqual(evonitoring.decide_alerting("14381234567", cfg),
+                         "twilio")
+        self.assertEqual(evonitoring.decide_alerting("15140987654", cfg),
+                         "twilio")
 
 
 if __name__ == '__main__':
